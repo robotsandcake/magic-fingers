d3674877badb7a468053e4136fe32a7efac851b1
Kevin Whinnery <kevin.whinnery@gmail.com>
2016-12-30 13:13:22 -0600

    change command name

--- commands.js
+++ commands.js
@@ -4,5 +4,5 @@ module.exports = {
   // This is a mapping of text that can be in an SMS, which will trigger 
   // A given IFTTT event
   red: 'red_flash_all',
-  blue: 'blue_lights',
+  blue: 'blue_flash',
 };
