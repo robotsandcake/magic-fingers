f1e1e479c3cf78101f72c25e058e245657d5d8ce
escapologybb <stuart@escapologybb.com>
2016-12-31 12:43:24 +0000

    Updated commands so they are uppercase

--- commands.js
+++ commands.js
@@ -3,6 +3,6 @@
 module.exports = {
   // This is a mapping of text that can be in an SMS, which will trigger 
   // A given IFTTT event
-  helping: 'red_flash_all',
-  assistance: 'blue_flash',
+  Helping: 'red_flash_all',
+  Assistance: 'blue_flash',
 };
